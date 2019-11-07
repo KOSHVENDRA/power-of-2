0ab1d8d3e977e90275440b01723903be0ed5e2a3
KOSHVENDRA SINGH <koshvendra1999@gmail.com>
2019-11-07 00:31:06 -0600

    using in operator

--- power.py
+++ power.py
@@ -1,9 +1,8 @@
 L = [1,2,4,8,16,32]
 X = 5
 
-for i in range(len(L)):
-    if 2 ** X == L[i]:
-         print('at index', i)
-         break
+if 2**X in L:
+    print('at index',L.index(2**X))
+         
 else:
     print(X, 'not found')
